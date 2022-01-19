0a92b25eb3eecdda9173856b9a92c28c451c147c
ilyes58 <55973481+ilyes58@users.noreply.github.com>
2022-01-19 11:09:11 -0500

    test commit et push

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ img_ref = cv2.imread('./ressources/Chambre/Reference.jpg', 0)
 img_ref = cv2.resize(img_ref, (600, 400))
 img = cv2.imread('./ressources/Chambre/IMG_6570.jpg', 0)
 #img = cv2.imread('./ressources/Cuisine/IMG_6563.jpg', 0)
-img = cv2.resize(img, (600, 400))
+img = cv2.resize(img, (600, 500))
 
 # --- affichage images ---
 """
