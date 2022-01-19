1c92fba8bdcd773025c1842a18e2ccfeaf70f0d7
PIER0318 <lefebvre.pierre0318@gmail.com>
2022-01-19 08:57:45 -0500

    ressources

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import cv2
 
 # --- lecture images ---
 img_ref = cv2.imread('./ressources/Chambre/Reference.jpg', 0)
+#img_ref = cv2.imread('./ressources/Cuisine/Reference.jpg', 0)
 img_ref = cv2.resize(img_ref, (600, 400))
 img = cv2.imread('./ressources/Chambre/IMG_6570.jpg', 0)
+#img = cv2.imread('./ressources/Cuisine/IMG_6563.jpg', 0)
 img = cv2.resize(img, (600, 400))
 
 # --- affichage images ---
@@ -21,8 +23,8 @@ img = cv2.equalizeHist(img)
 """
 
 diff = (img - img_ref)**2
-diff_blurred = cv2.GaussianBlur(diff, (21, 21), 5)
-ret, thresh = cv2.threshold(diff_blurred, 70, 255, cv2.THRESH_BINARY)
+diff_blurred = cv2.GaussianBlur(diff, (21, 21), 3)
+ret, thresh = cv2.threshold(diff_blurred, 80, 255, cv2.THRESH_BINARY)
 
 cv2.imshow('treshold', thresh)
 cv2.waitKey(0)
