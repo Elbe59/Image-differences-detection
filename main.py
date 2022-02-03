702124422c47f7145322102d5d582c251a1d8e64
Tom FERCOQ <tom.fercoq@outlook.fr>
2022-02-03 03:15:20 -0500

    improvement

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os.path
-
-import cv2
 import argparse
+import cv2
+import numpy as np
 
 # Parser
 parser = argparse.ArgumentParser()
@@ -18,25 +18,27 @@ img2 = cv2.imread(os.path.abspath(args.image2))
 diff = cv2.absdiff(img1, img2)
 gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
 blur = cv2.GaussianBlur(gray, (5, 5), 0)
-_, thresh = cv2.threshold(blur, 75, 255, cv2.THRESH_BINARY)
-dilated = cv2.dilate(thresh, None, iterations=50)
-cv2.namedWindow("Test", cv2.WINDOW_NORMAL)
-cv2.imshow("Test", thresh)
+_, thresh = cv2.threshold(blur, 100, 255, cv2.THRESH_BINARY)
+kernel = np.ones((5, 5), np.uint8)
+erode = cv2.erode(thresh, kernel, iterations=5)
+dilated = cv2.dilate(erode, kernel, iterations=50)
 contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-# for contour in contours:
-#     (x, y, w, h) = cv2.boundingRect(contour)
-#
-#     if cv2.contourArea(contour) < 900:
-#         continue
-#
-#     cv2.rectangle(img2, (x, y), (x + w, y + h), (0, 255, 0), 10)
-#
-# # Create window
-# cv2.namedWindow("Detection", cv2.WINDOW_NORMAL)
-#
-# # Show contours on window
-# cv2.imshow("Detection", img2)
+for contour in contours:
+    if cv2.contourArea(contour) < 900:
+        continue
+
+    (x, y, w, h) = cv2.boundingRect(contour)
+
+    cv2.rectangle(img1, (x, y), (x + w, y + h), (0, 255, 0), 10)
+    cv2.rectangle(img2, (x, y), (x + w, y + h), (0, 255, 0), 10)
+
+# Show the spotted differences
+height, width, _ = img1.shape
+x = np.zeros((height, int(width * 0.01), 3), np.uint8)
+result = np.hstack((img1, x, img2))
+cv2.namedWindow("Spotted differences", cv2.WINDOW_NORMAL)
+cv2.imshow("Spotted differences", result)
 
 # Wait pressed any key for destroy window
 cv2.waitKey(0)
