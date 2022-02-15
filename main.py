e458b33ce883e7eb75b68e3351599f79e51ee0e3
Quentin <quentin.callens@student.junia.com>
2022-02-15 17:53:48 -0500

    Adding PX parameter for isOverlappingNew

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ CHAMBRE_REPO = '/Chambre'
 CUISINE_REPO = '/Cuisine'
 SALON_REPO = '/Salon'
 DIM_IMG = (600, 400)
-PX = 0 # Value to increase the area of the rectangle
+PX = 3  # Value to increase the area of the rectangle
+
 
 def imgLoad(repo):
     img_list = []
@@ -42,33 +43,38 @@ def sortBoundingBoxes(bb_array):
 
 
 def isOverlapping(cbb, bb):
-    if bb[0]-PX <= cbb[0] <= bb[2]+PX and bb[1]-PX <= cbb[1] <= bb[3]+PX:
+    if bb[0] - PX <= cbb[0] <= bb[2] + PX and bb[1] - PX <= cbb[1] <= bb[3] + PX:
         return True
-    if bb[0]-PX <= cbb[2] <= bb[2]+PX and bb[1]-PX <= cbb[1] <= bb[3]+PX:
+    if bb[0] - PX <= cbb[2] <= bb[2] + PX and bb[1] - PX <= cbb[1] <= bb[3] + PX:
         return True
-    if bb[0]-PX <= cbb[0] <= bb[2]+PX and bb[1]-PX <= cbb[3] <= bb[3]+PX:
+    if bb[0] - PX <= cbb[0] <= bb[2] + PX and bb[1] - PX <= cbb[3] <= bb[3] + PX:
         return True
-    if bb[0]-PX <= cbb[2] <= bb[2]+PX and bb[1]-PX <= cbb[3] <= bb[3]+PX:
+    if bb[0] - PX <= cbb[2] <= bb[2] + PX and bb[1] - PX <= cbb[3] <= bb[3] + PX:
         return True
-    if cbb[0]-PX <= bb[0] <= cbb[2]+PX and cbb[1]-PX <= bb[1] <= cbb[3]+PX:
+    if cbb[0] - PX <= bb[0] <= cbb[2] + PX and cbb[1] - PX <= bb[1] <= cbb[3] + PX:
         return True
-    if cbb[0]-PX <= bb[2] <= cbb[2]+PX and cbb[1]-PX <= bb[1] <= cbb[3]+PX:
+    if cbb[0] - PX <= bb[2] <= cbb[2] + PX and cbb[1] - PX <= bb[1] <= cbb[3] + PX:
         return True
-    if cbb[0]-PX <= bb[0] <= cbb[2]+PX and cbb[1]-PX <= bb[3] <= cbb[3]+PX:
+    if cbb[0] - PX <= bb[0] <= cbb[2] + PX and cbb[1] - PX <= bb[3] <= cbb[3] + PX:
         return True
-    if cbb[0]-PX <= bb[2] <= cbb[2]+PX and cbb[1]-PX <= bb[3] <= cbb[3]+PX:
+    if cbb[0] - PX <= bb[2] <= cbb[2] + PX and cbb[1] - PX <= bb[3] <= cbb[3] + PX:
         return True
     else:
         return False
 
+
 def isOverlappingNew(boundingBox1, boundingBox2):
     """
-    If one of the extremity of the first bounding box overlap one of the extremity of the second bounding box, the two bounding boxes are overlapping
+    If one of the extremity of the first bounding box overlap one of the extremity of the second bounding box, the two bounding boxes are overlapping.
+    Using the global value of PX which makes the bounding bo larger
     :param boundingBox1: The first bounding box - Rectangle [x1,y1,x2,y2] with (x1,y1) the coordinates of the left-bottom corner and (x2,y2) the coordinated of the right top corner.
     :param boundingBox2: The second bounding box - Rectangle [x1,y1,x2,y2] with (x1,y1) the coordinates of the left-bottom corner and (x2,y2) the coordinated of the right top corner.
     :return: True if the two bounding boxes are overlapping. Else otherwise
     """
-    if(boundingBox2[0] >= boundingBox1[2]) or (boundingBox2[2] <= boundingBox1[0]) or (boundingBox2[3] <= boundingBox1[1]) or boundingBox2[1] >= boundingBox1[3]:
+    new_boundingBox1 = [boundingBox1[0] - PX, boundingBox1[1] - PX, boundingBox1[2] + PX, boundingBox1[3] + PX]
+    new_boundingBox2 = [boundingBox2[0] - PX, boundingBox2[1] - PX, boundingBox2[2] + PX, boundingBox2[3] + PX]
+    if (new_boundingBox2[0] >= new_boundingBox1[2]) or (new_boundingBox2[2] <= new_boundingBox1[0]) or (
+            new_boundingBox2[3] <= new_boundingBox1[1]) or (new_boundingBox2[1] >= new_boundingBox1[3]):
         return False
     else:
         return True
@@ -86,11 +92,11 @@ def threshMask(img, img_ref):
     abs_diff = cv2.absdiff(img_ref_grey, img_grey)
 
     # Gaussian Blur
-    struct_diff = cv2.GaussianBlur(struct_diff, (3, 3), cv2.BORDER_DEFAULT) # à mettre avant diff?
+    struct_diff = cv2.GaussianBlur(struct_diff, (3, 3), cv2.BORDER_DEFAULT)  # à mettre avant diff?
     abs_diff = cv2.GaussianBlur(abs_diff, (3, 3), cv2.BORDER_DEFAULT)
 
     # Threshold
-    struct_thresh = cv2.threshold(struct_diff, 50, 255, cv2.THRESH_BINARY_INV)[1] # adaptiveThreshold
+    struct_thresh = cv2.threshold(struct_diff, 50, 255, cv2.THRESH_BINARY_INV)[1]  # adaptiveThreshold
     abs_thresh = cv2.threshold(abs_diff, 50, 255, cv2.THRESH_BINARY)[1]
 
     # erode/dilate
@@ -115,19 +121,18 @@ def threshMask(img, img_ref):
 
 
 def makeBoundingBoxes(thresh):
-
     edges = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)[0]
 
     bb_array = []
     for i in range(len(edges)):
         x, y, w, h = cv2.boundingRect(edges[i])
-        bb_array.append([x, y, x+w, y+h])
-
+        bb_array.append([x, y, x + w, y + h])
+    print(bb_array)
     sortBoundingBoxes(bb_array)
-    #print(bb_array)
+    print(bb_array)
 
     bb_array = cv2.groupRectangles(np.concatenate((bb_array, bb_array)), groupThreshold=1, eps=0.2)[0]
-    #print(bb_array)
+    print(bb_array)
 
     return bb_array
 
