8429e8a21826b8af2bacdb8e0d89dd7b3fb47eef
PIER0318 <lefebvre.pierre0318@gmail.com>
2022-02-17 00:11:16 -0500

    métriques + overlapping

--- test.py
+++ test.py
@@ -3,9 +3,62 @@ from skimage.metrics import structural_similarity
 import numpy as np
 import cv2
 from _main import *
+import pandas as pd
+import json
+import torch
+import torchvision.ops.boxes as bops
 
-cbb = [0, 0, 10, 10]
-bb = [2, 2, 6, 6]
+"""
+box1 = torch.tensor([[511, 41, 577, 76]])
+box2 = torch.tensor([[544, 59, 610, 94]])
+iou = bops.box_iou(box1, box2)
 
-print(isOverlapping(bb, cbb))
+print(iou.item())
+"""
+
+
+def confusion_matrix(bb, lb):
+    seuil = 0.5
+    bb_count = [False] * len(bb)
+    lb_count = [False] * len(lb)
+
+    for i in range(len(bb)):
+        for j in range(len(lb)):
+            rec1 = [bb[i][0], bb[i][1], bb[i][0] + bb[i][2], bb[i][1] + bb[i][3]]
+            rec2 = [lb[i][0], lb[i][1], lb[i][0] + lb[i][2], lb[i][1] + lb[i][3]]
+            if bops.box_iou(torch.tensor([rec1]), torch.tensor([rec2])).item() > seuil:
+                bb_count[i] = True
+                lb_count[i] = True
+
+    tp = sum(bb_count)
+    fp = len(bb_count) - sum(bb_count)
+    fn = len(lb_count) - sum(lb_count)
+    nb_predict = len(bb)
+
+    return tp, fp, fn, nb_predict
+
+
+def calc_metrics(tp, fp, fn, nb_predict):
+    accuracy = tp / nb_predict
+    recall = tp / (tp + fn)
+    precision = tp / (tp + fp)
+    f1_score = (2 * precision * recall) / (precision + recall)
+    return accuracy, recall, precision, f1_score
+
+bb = [[0, 0, 10, 10], [20, 0, 5, 5]]
+lb = [[0, 0, 8, 8], [0, 20, 5, 5]]
+tp, fp, fn, nb_predict = confusion_matrix(bb, lb)
+accuracy, recall, precision, f1_score = calc_metrics(tp, fp, fn, nb_predict)
+
+print("tp = ", tp)
+print("fp = ", fp)
+print("fn = ", fn)
+print("nb_predict = ", nb_predict)
+print("accuracy = ", accuracy)
+print("recall = ", recall)
+print("precision = ", precision)
+print("f1_score = ", f1_score)
+
+
+
 
